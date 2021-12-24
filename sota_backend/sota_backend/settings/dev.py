9bfb13cd9f0f35ebffe28f1de3b8d72b2c2271d6
Pinzaru Iulian <pinzaru.iulian.igor@gmail.com>
2021-12-24 12:51:53 -0800

    proxy requesting

--- sota_backend/sota_backend/settings/dev.py
+++ sota_backend/sota_backend/settings/dev.py
@@ -11,7 +11,9 @@ CORS_ALLOWED_ORIGINS = [
     # "https://sub.example.com",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://24.21.90.246:8000"
+    "http://24.21.90.246:8000",
+    "http://192.168.0.23:8000",
+    "http://sotabackgrounds:8000"
 ]
 
 REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {  # 86,400 seconds in a day
